c3a7551f5c526667b229a304767ddde6eaeaa1b5
John Victor <johnvictorio3@gmail.com>
2019-09-23 11:50:10 -0300

    Create Car Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Car from './Car';
 
 function App() {
-  return <div>Hello World!</div>;
+  // return <Car />;
+  return (
+    <div>
+      <Car name="Jetão" />
+      <Car name="Celtinha" />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Car.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Car extends Component {
+  state = {
+    velocidade: 0,
+    peso: 3466,
+    cavalo: 3434
+  };
+
+  acelerar() {
+    this.setState(state => {
+      return {
+        velocidade: state.velocidade + 10
+      };
+    });
+  }
+
+  desacelerar() {
+    // TODO tarefa de casa
+  }
+
+  render() {
+    const { velocidade } = this.state;
+    const { name } = this.props;
+
+    return (
+      <div>
+        oi, eu sou o carro {name} e estou a {velocidade} km/h
+        <button onClick={() => this.acelerar()}>Acelerar</button>
+      </div>
+    );
+  }
+}
+
+export default Car;
